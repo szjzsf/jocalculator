ee2985d11379fd9838f2596df8d52e482d2245da
szjzsf <90899903+szjzsf@users.noreply.github.com>
2021-10-16 22:09:36 +0200

    Create .c

---
+++ .c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main () {
+    
+    double num1;
+    double num2;
+    char op;
+
+    printf("I'rj be egy szamot: ");
+    scanf("%lf", &num1);
+    printf("Va'lassz egy m'u'veletet: ");
+    scanf(" %c", &op);
+    printf("I'rj be me'gegy sza'mot: ");
+    scanf("%lf", &num2);
+
+    if(op == '+') {
+        printf("%f", num1 + num2);
+    } else if(op == '-') {
+        printf("%f", num1 - num2);
+    } else if(op == '*') {
+        printf("%f", num1 * num2);
+    } else if(op == '/') {
+        printf("%f", num1 / num2);
+    } else {
+        printf("Nem jo', van valami gebasz a rendszerben");
+    }
+return 0;
+}
